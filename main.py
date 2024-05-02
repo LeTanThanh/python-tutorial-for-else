c755a8c5133782c13f31c3bdca5510934c447c46
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-02 10:30:52 +0700

    Introduction to the Python for else statement

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 if __name__ == "__main__":
-  print("Python for else")
+  # Introduction to the Python for else statement
+
+  """
+  In Python, the for statement can have an optional else clause, which you may not be familiar with especially if you're coming from other languages such as Java or C#.
+  The following shows the syntax of the for statement with the else clause:
+
+  for item in iterables:
+    # process item
+  else:
+    # statement
+
+  In this syntax, the else clause will execute only if the loop runs normally.
+  In other words, the else clause won't execute if the loop encounters a break statement.
+  In addtional, the else clause also executes when the iterables object has no item.
+  The following flowchart illustrates the for ... else statement:
+
+  The else clause is quite useful in some cases if you know how to apply it effectively.
+  """
